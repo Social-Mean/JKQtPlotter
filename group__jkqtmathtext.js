225c24a6fce08001bcd61764d585d2d56d996b88
jkriege2 <jan@jkrieger.de>
2022-08-20 14:32:47 +0000

    Deploying to gh-pages from  @ 8b18de6082db332d49509f7ff05c7f2d73d9e851 🚀

--- group__jkqtmathtext.js
+++ group__jkqtmathtext.js
@@ -2,7 +2,6 @@ var group__jkqtmathtext =
 [
     [ "General Information", "group__jkqtmathtext__general.html", "group__jkqtmathtext__general" ],
     [ "Library-internal Types and Functions", "group__jkqtmathtext__interaltools.html", "group__jkqtmathtext__interaltools" ],
-    [ "Math Markup Parser Classes", "group__jkqtmathtext__parser.html", "group__jkqtmathtext__parser" ],
     [ "Math Renderer Class (JKQTMathText)", "group__jkqtmathtext__render.html", "group__jkqtmathtext__render" ],
     [ "Widgets using JKQTMathText", "group__jkqtmathtext__widgets.html", "group__jkqtmathtext__widgets" ]
 ];
